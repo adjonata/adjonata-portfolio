d71af42f11808366be95df9b817c3c43398cb6ef
adjonata <alexdjonata30@gmail.com>
2021-11-11 19:00:43 -0300

    fix: eslint and prettier config

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,27 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <button @click="alerta()">Alert</button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { defineComponent } from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue'
 
 export default defineComponent({
-  name: "Home",
+  name: 'Home',
   components: {
     HelloWorld,
   },
-});
+  setup(props, context) {
+    const alerta = () => {
+      alert('Teste')
+    }
+
+    return {
+      alerta,
+    }
+  },
+})
 </script>
