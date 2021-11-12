f4848e41309970a6b031cb22554b401782211006
adjonata <alexdjonata30@gmail.com>
2021-11-12 12:32:02 -0300

    fix: layout components

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer>Eu sou o Footer</footer>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,50 @@
+<template>
+  <header class="header">
+    <div class="header__content">
+      <h1>{{ title }}</h1>
+      <ul>
+        <li>{{ count.knowledges }}</li>
+        <li>{{ count.projects }}</li>
+      </ul>
+    </div>
+    <div class="header__image">
+      <img :src="`https://github.com/${githubUser}.png`" alt="Github Image" />
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType, ref } from 'vue'
+
+interface CountProp {
+  projects: number
+  knowledges: number
+}
+
+export default defineComponent({
+  props: {
+    count: {
+      type: Object as PropType<CountProp>,
+      required: true,
+    },
+  },
+  setup() {
+    const [title, githubUser] = [
+      ref('A credibilidade é consequência do método'),
+      ref('adjonata'),
+    ]
+
+    return { title, githubUser }
+  },
+})
+</script>
+
+<style scoped lang="scss">
+@import '~/src/assets/variables.scss';
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
