3407a535b704670b158888c74434c59a0e3929ba
adjonata <alexdjonata30@gmail.com>
2021-11-22 14:13:40 -0300

    fix: added color in sections

--- src/utils/sections.ts
+++ src/utils/sections.ts
@@ -1,6 +1,7 @@
 interface Section {
   title: string
   icon: string
+  color: string
   id: string
 }
 
@@ -8,21 +9,25 @@ const routes: Section[] = [
   {
     title: 'Sobre mim',
     icon: 'person',
+    color: '#3174cc',
     id: 'about',
   },
   {
     title: 'Conhecimentos',
     icon: 'account_balance',
+    color: '#ccb231',
     id: 'knowledges',
   },
   {
     title: 'Projetos',
     icon: 'dashboard',
+    color: '#6c31cc',
     id: 'projects',
   },
   {
     title: 'Contatos',
     icon: 'phone',
+    color: '#cc314b',
     id: 'social',
   },
 ]
